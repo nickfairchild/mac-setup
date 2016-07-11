7958460d1ac1461eef32e34d9bd3ef55aafffa52
Nick Fairchild <nick@wearethunderbolt.com>
2016-07-11 21:04:03 +0100

    main file

--- mac.sh
+++ mac.sh
@@ -1,83 +1,30 @@
 #!/usr/bin/env bash
 
-# Apps to install
-source apps/brews.sh
-source apps/casks.sh
-source apps/npms.sh
+source lib/installer.sh
+source lib/functions.sh
 
-source scripts/functions.sh
-
-source settings/git.sh
-
-# Install or upgrade Homebrew
-if test ! $(which brew); then
-    echo "Installing Xcode command line tools ..."
-    xcode-select --install
-
-    echo "Installing Homebrew ..."
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-else
-    echo "Homebrew already installed"
-    echo "Updating Homebrew ..."
-    brew update
-    brew upgrade
-fi
-
-brew doctor
-brew tap homebrew/dupes
-brew tap caskroom/cask
-
-fails=()
-
-echo "Installing brews ..."
-brew info ${brews[@]}
-install 'brew install' ${brews[@]}
-
-echo "Tapping casks ..."
-brew cask info ${casks[@]}
-install 'brew cask install' ${casks[@]}
-
-# Install or update composer
-if test ! $(which composer); then
-    echo "Installing composer ..."
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php -r "if (hash_file('SHA384', 'composer-setup.php') === '92102166af5abdb03f49ce52a40591073a7b859a86e8ff13338cf7db58a19f7844fbc0bb79b2773bf30791e935dbd938') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-    php composer-setup.php
-    php -r "unlink('composer-setup.php');"
-    if [ ! -d "/usr/local/bin/" ]; then
-        mkdir -p "/usr/local/bin"
-    fi
-    mv composer.phar /usr/local/bin/composer
-else
-    echo "Composer already installed"
-    echo "Updating composer ..."
-    composer self-update
-fi
-
-echo "Installing npms ..."
-install 'npm install --global' ${npms[@]}
-
-echo "Setting git defaults ..."
-for config in "${git_configs[@]}"
-do
-    git config --global ${config}
+while true; do
+  if [[ $# == 0 ]]; then
+    printf "\nUsage: mac OPTION\n"
+    printf "\nOSX Options:\n"
+    printf "  Setup:\n"
+    printf "    h:  Install Homebrew and casks.\n"
+    printf "    n:  Install npms.\n"
+    printf "    c:  Install composer.\n"
+    printf "    hs: Install homestead.\n"
+    printf "    z:  Install oh-my-zsh.\n"
+    printf "    d:  Apply OSX defaults.\n"
+    printf "    s:  Setup and configure installed software.\n"
+    printf "    df: Symlink dotfiles.\n"
+    printf "    i:  Install everything (i.e. executes all options, listed above, top to bottom).\n"
+    printf "  Manage:\n"
+    printf "    cs: Check status of managed software.\n"
+    printf "    C:  Caffeinate machine.\n"
+    printf "    q:  Quit/Exit.\n\n"
+    read -p "Enter Selection: " response
+    printf "\n"
+    process_option $response
+  else
+    process_option $1
+  fi
 done
-
-# Clean up
-echo "Cleaning up ..."
-brew cleanup
-brew cask cleanup
-
-for fail in ${fails[@]}
-do
-    echo "Failed to install: $fail"
-done
-
-# Install Oh-my-Zsh
-if test ! $(which zsh); then
-    brew install zsh
-    echo "Installing Oh-my-Zsh ..."
-    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
-
-echo "Done!"
